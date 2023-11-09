f7a66b574eea7314c954417637e5b9deb94b763c
ugwuoke ifeoma <tify68@gmail.com>
2023-11-08 16:32:48 -0800

    first commit

---
+++ JS/index.js
@@ -0,0 +1,100 @@
+// console.log( 1+1);
+function remote(){
+    prompt('how old are you?');
+} 
+// const temperature = 20
+// console.log(temperature);
+
+// let temperature =20
+// console.log(temperature);
+
+// let name = 'adams'
+// console.log(name);
+
+// let APC = false
+// console.log(APC);
+
+
+// let farook;
+// console.log(farook);
+
+// let chesaelgoals =null
+// console.log();
+
+// console.log(1+1);
+// console.log(2*4);
+// console.log(8/2);
+// console.log(5 ** 2);
+
+const colours =[ 'red', 'black', 'yellow', 'purple', 'blue',]
+// colours.pop();
+// console.log(colours);
+
+// colours.push('violet')
+// console.log(colours);
+
+// colours.shift();
+// console.log(colours);
+
+
+// colours.unshift('green');
+// console.log(colours);
+
+// const properties = [
+//    ' car', 'trucks',
+//    houses = ['bungalow', 'duplex', 'penthouse'],
+//    'yatch',
+//    watches = ['rolex', 'porshe', 'tag heurs']
+// ]
+// // console.log(houses[2]);
+// // console.log(watches[1]);
+// console.log(properties [2][2]);
+
+// const music = [
+//     {
+//     name: 'davido',
+//     age: 31,
+//     country :' nigeria',
+//     retired: false,
+// },
+// {
+//     name: 'drake',
+//     age: 36,
+//     country :' America',
+//      retired: false,
+// },
+// {
+//     name: 'rihanna',
+//     age: 35,
+//     country :' barbados',
+//     retired: false,
+// },
+// {
+//     name: 'tiwa savage',
+//     age: 42,
+//     country : 'nigeria',
+//     retired: false,
+// }
+// ]
+// console.log(music[2].name, music[2].age)
+// console.log(music[3].name, music[3].age, music[3].retired)
+
+// let x = "google"
+// let z = `check out this page ${x}, its a cool website`
+// console.log(z)
+
+// function java (a,b,c){
+//     return a + b -c
+// }
+// console.log(java(2,7,3));
+
+let a = 5
+if(a > 7){
+    console.log('Hello world');
+}
+else if (a = 6){
+    console.log('Hello Mar');
+}
+else{
+    console.log('Hello');
+}
